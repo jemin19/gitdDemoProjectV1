2267350f1a327471d2dc745d0051fb80efdeec38
Jemin Patel <jemin.patel19@gmail.com>
2021-01-13 00:01:57 -0500

    first commit

---
+++ src/test1.java
@@ -0,0 +1,13 @@
+
+public class test1 {
+	public static void main(String[] arg) {
+		String hungry = "yes"; 
+		
+		if(hungry == "yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("DO YOUR JOB");
+		}
+	}
+}
